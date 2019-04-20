3d1f082ebbb669ab4c77d4069ff13b7b4004456d
Huseyin ABANOZ <huseyinabanox@gmail.com>
2019-04-20 10:17:17 +0300

    src added

---
+++ src/main.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify
+from flask import abort
+from flask import request
+from tinydb import TinyDB
+
+app = Flask(__name__)
+
+@app.route('/trd/stats/add', methods=['POST'])
+def add_stats():
+    if not request.json or not 'stats' in request.json:
+        abort(400)
+
+    db.insert(request.json)
+
+    return jsonify({'stats': 'ok'}), 201
+
+@app.errorhandler(400)
+def uflr(e):
+    return e, 400
+
+
+if __name__ == '__main__':
+    db = TinyDB('db.json')
+    app.run(host='127.0.0.1', port=8080,debug=True)
